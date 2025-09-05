e133b641e8c7157be476174e3bbd7f0eb8903fa8
tail1887 <tail1887@users.noreply.github.com>
2025-09-05 10:42:29 +0000

    deploy: 1ea796e1483fa1f305da9a8c527c660deb48e008

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  testEnvironment: 'node', // ← jsdom 대신 node로 변경
-  transform: {
-    '^.+\\.m?[jt]sx?$': ['babel-jest', { configFile: './babel.config.js' }]
-  }
-};
--- scripts/app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // 앱 초기화 코드
-  console.log('Offline Diary 앱 시작');
-});
